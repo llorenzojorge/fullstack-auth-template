69ae45a57d00d77d60f3d57455e7b35912ecfc0c
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2026-04-17 17:48:40 -0300

    Middleware de Autorização

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -2,11 +2,16 @@ import { Router } from "express"
 import { ProductsController } from "@/controllers/products-controller"
 
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+// Aplicar autorização em todas as rotas.
+// productsRoutes.use(verifyUserAuthorization(["sale", "admin"]))
+
 productsRoutes.get("/", productsController.index)
-productsRoutes.post("/", ensureAuthenticated, productsController.create)
+// Autorização em uma rota especifica.
+productsRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["sale", "admin"]), productsController.create)
 
 export { productsRoutes }
